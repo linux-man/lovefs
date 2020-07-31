ada134371ea9f60eb50af9ccac06508894c79d3d
David Konsumer <konsumer@jetboystudio.com>
2020-07-31 13:15:03 -0700

    add readlink and test.lua, add directory/file support to ls()

---
+++ test.lua
@@ -0,0 +1,15 @@
+-- run with luajit test.lua
+
+require "lovefs.lovefs"
+
+local fs = lovefs("/")
+
+local path, tDirs, tFiles, tAll = fs:ls()
+
+for i,v in pairs(tDirs) do
+    print(v..'/')
+end
+
+for i,v in pairs(tFiles) do
+    print(v)
+end
